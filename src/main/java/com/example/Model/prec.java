79e120f3425f54e9d22daff1770f771346216674
ibadBargir <ibad72086@gmail.com>
2025-09-23 22:49:30 +0530

    Add Api exampel for teacher project

---
+++ src/main/java/com/example/Model/prec.java
@@ -0,0 +1,39 @@
+package com.example.Model;
+
+public class prec {
+
+    public static void main(String[] args) {
+        /*
+        ******
+        ******
+        ******
+        ******
+*/
+        /*
+        ******
+        *    *
+        *    *
+        ******
+        */
+        int m=4;
+        int n=6;
+
+        for (int i =1; i<=m; i++){
+
+            for (int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        int  e=6;
+        int y=4;
+
+        for (int i =1; i<=e; i++){
+            for (int j=1; j<=y;j++){
+                System.out.println(" ");
+            }
+            System.out.println("*");
+        }
+    }
+}
